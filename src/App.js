0d12bead493488d100d155e0b4bc94d06712f908
AmirMasoud Jafari <amirmasoodjafari1677@gmail.com>
2021-11-05 15:11:11 +0330

    updated the project with react router v5

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { Switch, Route } from 'react-router';
 import HomePage from './pages/homepage.component';
 import './App.css';
 
 function App() {
+
+
+  const HatPage = () => {
+    <h1>Hat Page</h1>
+  }
+
+  
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hat' component={HatPage} />
+      </Switch>
+      
     </div>
   );
 }
--- src/pages/homepage.component.jsx
+++ src/pages/homepage.component.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import Directory from '../components/directory/directory.component';
-import MenuItem from '../components/menu-item/menu-item.component'
 import './homepage.styles.scss';
 
- const HomePage = () => (
-    <div className="homepage">
-            <Directory />
-
-    </div>
+const HomePage = () => (
+        <div className="homepage">
+                <Directory />
+        </div>
 )
 
 export default HomePage;
